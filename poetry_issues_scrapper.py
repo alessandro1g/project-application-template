3bf1b97a68e2e2203da40dab781155feeeac3912
alessandro1g <97135667+alessandro1g@users.noreply.github.com>
2025-04-14 21:43:45 -0400

    updated

--- poetry_issues_scrapper.py
+++ poetry_issues_scrapper.py
@@ -6,7 +6,7 @@ import time
 # GitHub repository issues API endpoint
 repo_url = "https://api.github.com/repos/python-poetry/poetry/issues"
 
-access_token = "nope"
+access_token = ""
 headers = {
     "Authorization": f"token {access_token}"
 }
@@ -18,7 +18,7 @@ def fetch_all_issues(repo_url, headers):
     page = 1
     while True:
         # Add pagination parameters
-        response = requests.get(repo_url, headers=headers, params={"per_page": 100, "page": page})
+        response = requests.get(repo_url, headers=headers, params={"per_page": 1000, "page": page})
         time.sleep(1)
         print(f"Fetching page {page}...")
         if response.status_code != 200:
@@ -32,10 +32,11 @@ def fetch_all_issues(repo_url, headers):
     print(f"Fetched {len(issues)} issues.")
     return issues
 
-def fetch_issue_timeline(url): 
+def fetch_issue_timeline(url,count): 
     page = 1
+    print(count)
     while True:
-        response = requests.get(repo_url, headers=headers, params={"per_page": 50, "page": 1})
+        response = requests.get(url, headers=headers, params={"per_page": 1000, "page": page})
         time.sleep(1)
         print(f"Fetching page {page}...")
         if response.status_code != 200:
@@ -63,11 +64,12 @@ def fetch_issue_timeline(url):
 # Fetch all issues
 issues = fetch_all_issues(repo_url, headers)
 
+count = 0
 for issue in issues: 
-
+    count += 1
     big_issues.append({
         "url": issue["url"],
-        "creator": issue["user"]["login"],
+        "creator": issue.get("user", {}).get("login", "N/A"),  # Safely access the "login" key
         "labels": [label["name"] for label in issue["labels"]],
         "state": issue["state"],
         "assigness": [assignee["login"] for assignee in issue["assignees"]],
@@ -76,11 +78,11 @@ for issue in issues:
         "number": issue["number"],
         "created_date": issue["created_at"],
         "updated_date": issue["updated_at"],
+        "closed_date": issue["closed_at"],
         "timeline_url": issue['timeline_url'],
-        "events": fetch_issue_timeline(issue['timeline_url']),
+        "events": fetch_issue_timeline(issue['timeline_url'], count),
         "response_count": issue.get("comments", 0)  # Add the number of comments
     })
-
 # Save the extracted issues to a JSON file
 with open('github_issues.json', 'w') as json_file:
     json.dump(big_issues, json_file, indent=4)
